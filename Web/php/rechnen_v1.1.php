4360a1920c697eed789e3f734ae7565be09ea10d
[OmG] Network <webmaster@omg-network.de>
2018-06-08 15:17:43 +0200

    Add files via upload

---
+++ Web/php/rechnen_v1.1.php
@@ -0,0 +1,50 @@
+<?php
+
+$zahl1 = $_POST['zahl1'] ;
+$zahl2 = $_POST['zahl2'] ;
+$z = $_POST['z'] ;
+
+switch($z) {
+	case "+":
+	$erg = $zahl1 + $zahl2;
+	break;
+	
+	case "-":
+	$erg = $zahl1 - $zahl2;
+	break;
+	
+	case "*":
+	$erg = $zahl1 * $zahl2;
+	break;
+	
+	case "/":
+	$erg = $zahl1 / $zahl2;
+	break;
+	
+	default:
+	echo "ERROR";
+	break;
+}
+?>
+
+<html>
+	<head>
+		<title>PHP Berechnung</title>
+	</head>
+	<body>
+	 <h3>Hier können Sie Daten zum Berechnen eingeben</h3>
+	 <form action"rechnen.php" method="POST" enctype"">
+       <table>
+	    <tr><td>Zahl1: </td><td><input type="text" name="zahl1" size="30" value="55"></td></tr>
+		<tr><td>Zahl2: </td><td><input type="text" name="zahl2" size="30" value="66"></td></tr>
+	   </table>
+	 <p />
+	 <input type="redio" name="z" size="30" value="+">
+	 <input type="radio" name="z" size="30" value="-">
+	 <input type="radio" name="z" size="30" value="*">
+	 <input type="radio" name="z" size="30" value="/">
+	 <input type="submit" value="addieren">
+	</form>
+ Das Ergebniss ist: <?php echo $erg; ?>
+</body>
+</html>
